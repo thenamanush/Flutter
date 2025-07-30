f8b8c400eea33e3d6d8cca1bcd420c93f8e54a70
Bashar Numan <basharmohammadnoman@gmail.com>
2025-07-30 23:45:17 +0600

    new app

---
+++ practice/lib/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : Text('Home Page'),
+      ),
+      body: Center(child: Text('welcome to the homepage'),
+      ),
+    );
+  }
+}
